d5470b7c8e321174e1d2631c95d72ed5faaadc43
Yasser Osama <yosama@youxel.com>
2024-02-05 17:27:19 +0200

    fixing module erroor

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BarcodeScanner",
-            path: "Sources"
+            path: "Sources",
+            resources: [.process("Resources")]
         ),
     ]
 )
